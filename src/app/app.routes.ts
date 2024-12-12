de56022f42ac9a9d01e926951ed6c17ef5b8ca9a
nasih04 <dev.nasih@gmail.com>
2024-12-12 13:47:41 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'login', component: LoginComponent}
+];
